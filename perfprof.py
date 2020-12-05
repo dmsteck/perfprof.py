4983b20c81e139a649d9e455ae1a65777abe91f9
Daniel Steck <daniel@cloudnc.com>
2020-12-05 13:30:47 +0000

    fixes

--- perfprof.py
+++ perfprof.py
@@ -25,29 +25,41 @@ def thetaMax(data, minvals):
     return thmax
 
 
-def thetaColumn(col, minvals):
+def theta(col, minvals):
     """
     Performance ratios for an individual solver against the vector of minimum values.
     Division by Inf produces NaN.
     """
     assert np.all(minvals > 0)
     th = np.full(np.shape(col), np.nan)
-    th[minvals < np.inf] = col / minvals
+    valid = (minvals < np.inf)
+    th[valid] = col[valid] / minvals[valid]
     return th
 
 
-def makeStaircase(col, thmax, tol):
+def makeStaircase(col, m, thmax, tol):
+    """
+    Assemble staircase (x, y) pairs.
+    col : "column" of theta values
+    m : number of problems
+    thmax : maximum value of theta for endpoint clamping
+    tol : theta tolerance for endpoint clamping
+    """
     theta, counts = np.unique(col, return_counts=True)
-    r = len(theta)
-    prob = np.cumsum(counts) / len(col)    
-    # TODO: get rid of floating arange
-    k = np.array(np.floor(np.arange(0, r, 0.5)), dtype = np.int)
-    x = theta[k[1:]]
-    y = prob[k[:-1]]
-    return x, y
+    prob = np.cumsum(counts) / m
+
+    # Ensure endpoints plotted correctly
+    if theta[0] >= 1 + tol:
+        theta = np.append(1, theta)
+        prob = np.append(0, prob)
+    if theta[-1] < thmax - tol:
+        theta = np.append(theta, thmax)
+        prob = np.append(prob, prob[-1])
+
+    return theta, prob
 
 
-def perfprof(data, thmax = None, tol = np.sqrt(np.finfo(np.double).eps)):
+def perfprof(data, linestyle, thmax = None, tol = np.sqrt(np.finfo(np.double).eps), **kwargs):
     """
     Peformance profile for the input data.
 
@@ -56,12 +68,16 @@ def perfprof(data, thmax = None, tol = np.sqrt(np.finfo(np.double).eps)):
     data : Array of timings/errors to plot.
            M-by-N matrix where data[i, j] > 0 measures the performance of the
            j-th solver on the i-th problem, with smaller values denoting "better".
+    
+    linestyle : List of line specs, e.g., ['o-r', '-.g']
 
     thmax : Maximum value of theta shown on the x-axis.
             Defaults to max(tm, 1.01), where tm is the largest finite performance ratio.
 
     tol : Tolerance for endpoint clamping.
           Defaults to sqrt(eps), where eps is the double precision machine accuracy.
+
+    **kwargs : Optional keyword args to be forwarded to matplotlib.
     
     Returns
     -------
@@ -74,38 +90,35 @@ def perfprof(data, thmax = None, tol = np.sqrt(np.finfo(np.double).eps)):
     data = np.asarray(data).astype(np.double)
     m, n = data.shape  # `m` problems, `n` solvers
 
+    # Check input
+    if len(linestyle) != n:
+        raise ValueError("Number of line specs doesn't match number of solvers")
+
     # Row-wise minima. NaN values are treated like +infinity.
     minvals = np.min(data, axis=1, initial=np.inf, where=~np.isnan(data))
 
-    # Check for invalid performance measurements
     if np.any(minvals <= 0):
-        raise Exception("Data contains non-positive performance measurements")
+        raise ValueError("Data contains non-positive performance measurements")
 
     if thmax is None:
         thmax = thetaMax(data, minvals)
 
     h = [None] * n
     for solver in range(n):  # for each solver
-        col = thetaColumn(data[:, solver], minvals)  # performance ratio
+        col = theta(data[:, solver], minvals)  # performance ratio
         col = col[col <= thmax] # crop and remove infs/NaNs
 
-        x, y = makeStaircase(col, thmax, tol)
+        if len(col) == 0:
+            continue
 
-        # Ensure endpoints plotted correctly
-        if x[0] >= 1 + tol:
-            x = np.append([1, x[0]], x)
-            y = np.append([0, 0], y)
-        if x[-1] < thmax - tol:
-            x = np.append(x, thmax)
-            y = np.append(y, y[-1])
+        th, prob = makeStaircase(col, m, thmax, tol)
 
-        # plot current line
-        # TODO: use plt.step
-        h[solver] = plt.plot(x, y)
+        # plot current line and disable frame clipping (to support y-intercept marking)
+        h[solver] = plt.step(th, prob, linestyle[solver], where='post', **kwargs)
+        h[solver][0].set_clip_on(False)
 
-    # set xlim
+    # set axis limits
     plt.xlim([1, thmax])
     plt.ylim([0, 1.01])
-    plt.show()  #plt.draw()?
 
     return thmax, h
